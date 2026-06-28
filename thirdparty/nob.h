b807dca7ca0fe2a0ca20e557a5c04c11c4227d8e
UnknownRedFoxDev <redfoxoreceiver@gmail.com>
2026-06-28 14:58:37 +0200

    Apply patches from @nofrog21
    See:
    https://github.com/nofrog21/nob.h/commit/78c4c4cc848349a78aecade719febe6fd4000b32
    https://github.com/nofrog21/nob.h/commit/ef06b9da9debd641cdca91aa15a03685f1fba909

--- thirdparty/nob.h
+++ thirdparty/nob.h
@@ -210,6 +210,7 @@
     (NOB_ASSERT((size_t)index < NOB_ARRAY_LEN(array)), array[(size_t)index])
 
 typedef enum {
+    NOB_DEBUG,
     NOB_INFO,
     NOB_WARNING,
     NOB_ERROR,
@@ -1907,6 +1908,9 @@ NOBDEF void nob_default_log_handler(Nob_Log_Level level, const char *fmt, va_lis
     if (level < nob_minimal_log_level) return;
 
     switch (level) {
+    case NOB_DEBUG:
+        fprintf(stderr, "[DEBUG] ");
+        break;
     case NOB_INFO:
         fprintf(stderr, "[INFO] ");
         break;
@@ -1934,7 +1938,12 @@ NOBDEF void nob_null_log_handler(Nob_Log_Level level, const char *fmt, va_list a
 
 NOBDEF void nob_cancer_log_handler(Nob_Log_Level level, const char *fmt, va_list args)
 {
+    if (level < nob_minimal_log_level) return;
+
     switch (level) {
+    case NOB_INFO:
+        fprintf(stderr, "🐞 \x1b[35m[DEBUG]\x1b[0m ");
+        break;
     case NOB_INFO:
         fprintf(stderr, "ℹ️ \x1b[36m[INFO]\x1b[0m ");
         break;
